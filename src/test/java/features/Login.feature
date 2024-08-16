48f65cd9359430acffe61683690b2f79e0607264
Attent1 <rafaeldsc22@outlook.com>
2024-08-16 11:49:35 -0300

    first commit

---
+++ src/test/java/features/Login.feature
@@ -0,0 +1,8 @@
+# language: pt
+
+  Funcionalidade: Login
+
+    Cenario: login com sucesso
+    Dado que estou na tela de login
+    Quando faco login com credenciais validas
+    Entao vejo a tela de "Select Flight"
---
+++ src/test/java/stepDefinition/LoginSteps.java
@@ -0,0 +1,23 @@
+package stepDefinition;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+
+
+    @Dado("que estou na tela de login")
+    public void que_estou_na_tela_de_login() {
+
+    }
+    @Quando("faco login com credenciais validas")
+    public void faco_login_com_credenciais_validas() {
+
+    }
+    @Entao("vejo a tela de {string}")
+    public void vejo_a_tela_de(String string) {
+
+    }
+
+}
