d74b384d53733ce083cac2e9e182ec046a486e6e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-30 11:49:54 -0300

    Corrigindo Exercicio, não duplicar cenario

--- src/test/java/stepDefinition/SelectFlightSteps.java
+++ src/test/java/stepDefinition/SelectFlightSteps.java
@@ -9,10 +9,13 @@ import pages.SelectFlight;
 public class SelectFlightSteps extends BasePage {
 
     SelectFlight selectFlight = new SelectFlight();
+    LoginSteps login = new LoginSteps();
 
     @Dado("que estou na tela Select Flight")
     public void que_estou_na_tela_select_flight() {
-        selectFlight.isOnSelectFlightPage();
+        login.que_estou_na_tela_de_login();
+        login.entro_com_credenciais_validas();
+        login.vejo_a_tela_select_flight();
     }
     @Quando("clico no botao signoff")
     public void clico_no_botao_signoff() {
